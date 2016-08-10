883b63471e8750943e5672c7fe091324d0fe1dd6
Administrator <andreas.oberhack@spicter.com>
2016-08-10 06:59:38 +0200

    local multi node

--- spicteros/bin/deploy.sh
+++ spicteros/bin/deploy.sh
@@ -35,6 +35,28 @@ function deploy_single_local () {
   cd $bin_dir
   ansible-playbook $home_dir"/site.yml" --extra-vars "home_dir=$home_dir/"
 }
+
+#
+# ----------- deploy_multi_local ----------
+function deploy_multi_local () {
+  
+  local home_dir='../site/multi-node/vagrant'
+  local bin_dir=$(pwd)
+#  sh ../lib/generate_vagrant_ssh_config $home_dir"/hosts"
+  
+  if [ -n "$command" ]; then 
+     if [ "$command" = 'clean' ]; then
+        cd $home_dir
+        echo "******* destroy vm"
+        vagrant destroy --force
+        echo "******* fire up new vm"
+        vagrant up
+     fi
+  fi
+  cd $bin_dir
+#  ansible-playbook $home_dir"/site.yml" --extra-vars "home_dir=$home_dir/"
+}
+
 #
 # ------------------ Main -----------------
 case $target in
@@ -47,7 +69,7 @@ case $target in
    exit 0
    ;;
 "local-multi")
-   message="found"
+   deploy_multi_local
    exit 0
    ;;
 "remote-multi")
